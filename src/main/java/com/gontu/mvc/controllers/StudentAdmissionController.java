d325915f02ed7b0911f8b2b8d57e7c6dfbb5c96d
rajindersingh001 <rajinders@chetu.com>
2019-01-03 17:25:20 +0530

    step-12

--- src/main/java/com/gontu/mvc/controllers/StudentAdmissionController.java
+++ src/main/java/com/gontu/mvc/controllers/StudentAdmissionController.java
@@ -1,6 +1,7 @@
 package com.gontu.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,7 @@ public class StudentAdmissionController {
 	@RequestMapping(value="/admission-form", method = RequestMethod.GET)
 	public ModelAndView displayAdmissionForm() {
 		ModelAndView mv=new ModelAndView("admission-form");
-		mv.addObject("headerMessage", "Best College of Engineering, India");
+		//mv.addObject("headerMessage", "Best College of Engineering, India");
 		return mv;
 	}
 	
@@ -27,11 +28,14 @@ public class StudentAdmissionController {
 		mv.addObject("studentObj", stud);
 		return mv;
 	}*/
-	
+	@ModelAttribute
+	public void  addCommonObjects(Model model1) {
+		model1.addAttribute("headerMessage", "Best College of Engineering, India");
+	}
 	@RequestMapping(value="/submit-admission-form", method = RequestMethod.POST)
 	public ModelAndView submitAdmissionForm(@ModelAttribute("studentObj")Student stud ) {
 		ModelAndView mv=new ModelAndView("admission-success");
-		mv.addObject("headerMessage", "Best College of Engineering, India");
+		//mv.addObject("headerMessage", "Best College of Engineering, India");
 		return mv;
 	}
 }
